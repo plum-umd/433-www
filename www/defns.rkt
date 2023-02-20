a5441e2bd8c20c006756e2ab744a258fd69f57bb
Benjamin Roberts <benjaminrobertsplus1@gmail.com>
2023-02-20 11:58:54 -0500

    added oh for Mingwei and I

--- www/defns.rkt
+++ www/defns.rkt
@@ -27,9 +27,9 @@
 (define ghc-version "9.0.2")
 
 (define staff
-  (list (list "Pierce Darragh"  "pdarragh@umd.edu" "TBD")
-        (list "Benjamin Robers"  "bobert95@terpmail.umd.edu" "TBD")
-        (list "Mingwei Zhu"  "mzhu1@umd.edu" "TBD")
+  (list (list "Pierce Darragh"  "pdarragh@umd.edu" "Tu: 11am-1pm (virtual), W: 2pm-4pm, Th: 12pm-2pm (virtual)")
+        (list "Benjamin Roberts"  "bobert95@terpmail.umd.edu" "M: 10:30am-1:30pm, Tu: 9:30am-11am (virtual), Th: 9:30am-11am (virtual)")
+        (list "Mingwei Zhu"  "mzhu1@umd.edu" "M: 2pm-4pm, Tu 12:30pm-1:30pm 3:30pm-4:30pm, Th: 12:30pm-1:30pm 3:30pm-4:30pm")
         ))
 
 
