31960a178757eecdc658566e3f02cb8be210268d
hybras <24651269+hybras@users.noreply.github.com>
2023-03-29 15:04:52 -0400

    Port nix flake to {linux, darwin} x {arm, x86}

--- flake.nix
+++ flake.nix
@@ -4,18 +4,25 @@
   inputs.nixpkgs.url = "github:NixOS/nixpkgs/nixos-22.11";
 
   outputs = { self, nixpkgs }:
-    let pkgs = nixpkgs.legacyPackages.x86_64-linux;
-        ghcPkgs = pkgs.haskellPackages.ghcWithPackages (p: with p; [
-          HUnit
-        ]);
+    let
+    # Systems supported
+        allSystems = [
+          "x86_64-linux" # 64-bit Intel/AMD Linux
+          "aarch64-linux" # 64-bit ARM Linux
+          "x86_64-darwin" # 64-bit Intel macOS
+          "aarch64-darwin" # 64-bit ARM macOS
+        ];
+        forAllSystems = f: nixpkgs.lib.genAttrs allSystems (system: f {
+            pkgs = import nixpkgs { inherit system; };
+          });
     in {
-
-    devShell.x86_64-linux = pkgs.mkShell {
+    devShells = forAllSystems ({ pkgs }: {
+      default = pkgs.mkShell {
       buildInputs = [ pkgs.cargo
                       pkgs.cabal-install
                       pkgs.nasm
                       pkgs.racket
-                      ghcPkgs
+                      ( pkgs.haskellPackages.ghcWithPackages (p: with p; [ HUnit ]) )
                       pkgs.ocaml
                       pkgs.ocamlPackages.findlib
                       pkgs.ocamlPackages.utop
@@ -25,7 +32,7 @@
                       pkgs.texlive.combined.scheme-full
                       pkgs.zip
                     ];
-
     };
+    });
   };
 }
