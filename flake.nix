7c4ebba0cecbc0c272238c1ef95df6b1e051aadc
hybras <24651269+hybras@users.noreply.github.com>
2023-03-29 15:15:04 -0400

    Formatting

--- flake.nix
+++ flake.nix
@@ -5,34 +5,35 @@
 
   outputs = { self, nixpkgs }:
     let
-    # Systems supported
-        allSystems = [
-          "x86_64-linux" # 64-bit Intel/AMD Linux
-          "aarch64-linux" # 64-bit ARM Linux
-          "x86_64-darwin" # 64-bit Intel macOS
-          "aarch64-darwin" # 64-bit ARM macOS
-        ];
-        forAllSystems = f: nixpkgs.lib.genAttrs allSystems (system: f {
-            pkgs = import nixpkgs { inherit system; };
-          });
+      # Systems supported
+      allSystems = [
+        "x86_64-linux" # 64-bit Intel/AMD Linux
+        "aarch64-linux" # 64-bit ARM Linux
+        "x86_64-darwin" # 64-bit Intel macOS
+        "aarch64-darwin" # 64-bit ARM macOS
+      ];
+      forAllSystems = f:
+        nixpkgs.lib.genAttrs allSystems
+        (system: f { pkgs = import nixpkgs { inherit system; }; });
     in {
-    devShells = forAllSystems ({ pkgs }: {
-      default = pkgs.mkShell {
-      buildInputs = [ pkgs.cargo
-                      pkgs.cabal-install
-                      pkgs.nasm
-                      pkgs.racket
-                      ( pkgs.haskellPackages.ghcWithPackages (p: with p; [ HUnit ]) )
-                      pkgs.ocaml
-                      pkgs.ocamlPackages.findlib
-                      pkgs.ocamlPackages.utop
-                      pkgs.pandoc
-                      pkgs.rustc
-                      pkgs.rsync
-                      pkgs.texlive.combined.scheme-full
-                      pkgs.zip
-                    ];
+      devShells = forAllSystems ({ pkgs }: {
+        default = pkgs.mkShell {
+          buildInputs = [
+            pkgs.cargo
+            pkgs.cabal-install
+            pkgs.nasm
+            pkgs.racket
+            (pkgs.haskellPackages.ghcWithPackages (p: with p; [ HUnit ]))
+            pkgs.ocaml
+            pkgs.ocamlPackages.findlib
+            pkgs.ocamlPackages.utop
+            pkgs.pandoc
+            pkgs.rustc
+            pkgs.rsync
+            pkgs.texlive.combined.scheme-full
+            pkgs.zip
+          ];
+        };
+      });
     };
-    });
-  };
 }
